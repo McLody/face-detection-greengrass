f9b29578ea936529de4d073f98cae79459f33ee1
Jiachen Wu <mclodyw@gmail.com>
2022-11-10 14:46:28 +0800

    change secret

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,3 +1,4 @@
+
 import time
 import cv2
 import random
@@ -15,60 +16,36 @@ errTopic = 'detection_failed'
 
 secret_name = "greengrass-lambda-access-s3-secretkey"
 region_name = "us-east-1"
-secret = ''
+secret = []
 access_key_id=''
 access_secret_key=''
 
 
 
 def get_secret():
-    
     # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
     )
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
+
     try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
     except ClientError as e:
-        print(e)
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-    else:
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            global secret
-            secret = get_secret_value_response['SecretString']
-        else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    global secret
+    secret = get_secret_value_response
 
 get_secret()
-secret_json = json.loads(secret)
-access_key_id = secret_json['access_key_id']
-access_secret_key = secret_json['access_secret_key']
+access_key_id = secret['access_key_id']
+access_secret_key = secret['access_secret_key']
 
 s3 = boto3.client(
     's3',
